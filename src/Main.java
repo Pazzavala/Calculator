1f2eca583a928e9b5ecdd3403ea11f527f74b763
Maria Paz <pazrodriguez612@gmail.com>
2022-12-27 17:01:10 -0600

    initial Commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Frame: 420, 550
+        // null
+        // TextField: 50, 25, 300, 50
+        // DelButton: 50, 430, 145, 50
+        // ClearButton: 205, 420, 145, 50
+        // PanelTo Hold buttons: 50, 100, 300, 300 w/
+            // grid layout 4, 4, 10, 10 color: GRAY
+
+
+    }
+}
